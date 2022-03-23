fd82da82072bed6b0838f18b18143ead38ba1d79
Zhijun Song <songzhijun2015@gmail.com>
2022-03-23 08:14:34 -0700

    update permanent address

--- javascript/sketch.js
+++ javascript/sketch.js
@@ -1,183 +1,196 @@
-let sounds = [];
-let imports = [];
-let fileName=[];
-let backgroundImg=[];
-let total= 10;
-let nodeY = [];
+let mapdata=[
+  {
+    "location": "Canal Street",
+    "soundaddress": "https://github.com/ZhijunSong/MuseumofSound/blob/main/assets/Canal%20street/Canal%20St.02.mp3?raw=true",
+    "description": "Canal St-Chinese New Year Festivities: Stepping into Manhattan’s Chinatown is like being immersed in another country. During January or February, you can hear people celebrating lunar new year. A common festivity during this time is lighting firecrackers, which  were originally used to scare away evil spirits. You can hear children playing with bang snaps, a small firecracker that emitts a crisp sound when thrown on the ground.",
+    "pictures": "https://raw.githubusercontent.com/ZhijunSong/MuseumofSound/main/assets/Canal%20street/CanalSt2.jpg"
+  },
+  {
+    "location": "Canal Street : Supermarket",
+    "soundaddress": "https://github.com/ZhijunSong/MuseumofSound/blob/main/assets/Canal%20street/Canal%20St.04.m4a?raw=true",
+    "description": "Canal St-Chinatown Supermarket: There are many supermarkets in Chinatown where you can find authentic Asian food and ingredients. The supermarket transports shopper to the 1990s with this classic Cantonese song. The vintage decorations along with the tunes tell us a story from the last century.",
+    "pictures": "https://raw.githubusercontent.com/ZhijunSong/MuseumofSound/main/assets/Canal%20street/CanalSt4.jpg"
+  },
+  {
+    "location": "Canal Street : Supermarket",
+    "soundaddress": "https://github.com/ZhijunSong/MuseumofSound/blob/main/assets/Canal%20street/Canal%20St.03m4a?raw=true",
+    "description": "Canal St- Chinatown Restaurant: On a weekend morning, restaurants in Chinatown are bustling with diners. Here, you can hear the sound of people ordering food in Cantonese, a language characteristic of Chinatown. Historically, Cantonese speakers dominated the western side of Chinatown, an area that continues to be a main business district. This language represents the history of Chinese immigration.",
+    "pictures": "https://raw.githubusercontent.com/ZhijunSong/MuseumofSound/main/assets/Canal%20street/CanalSt3.jpg"
+  },
+  {
+    "location": "Canal Street",
+    "soundaddress": "https://github.com/ZhijunSong/MuseumofSound/blob/main/assets/Canal%20street/Canal%20St.01m4a?raw=true",
+    "description": "Canal St-Chinatown Er Hu Player: \nThe erhu is a two-stringed bowed musical instrument and is known in the West as the Chinese violin. It can be a solo instrument or played in small emsembles or large orchestras.  Unlike street performers playing western instruments in Time square or Union square, elderly performers in Chinatown bring Chinese traditional sounds to this diverse city.",
+    "pictures": "https://raw.githubusercontent.com/ZhijunSong/MuseumofSound/main/assets/Canal%20street/Canal%20St.%2001.png"
+  },
+  {
+    "location": "Canal Street",
+    "soundaddress": "https://github.com/ZhijunSong/MuseumofSound/blob/main/assets/Canal%20street/Canal%20St.05.m4a?raw=true",
+    "description": "Canal St-Chinatown Antique Store: This store is located in Chinatown and sells Chinese trinkets and souvenirs. In this clip, you can hear the bells from a wind chime, a sound commonly heard in this area.",
+    "pictures": "https://github.com/ZhijunSong/MuseumofSound/blob/main/assets/Canal%20street/Canal%20St.%2005.HEIC?raw=true"
+  },
+  {
+    "location": "Union Square",
+    "soundaddress": "https://github.com/ZhijunSong/MuseumofSound/blob/main/assets/14th%20street/14th%20St.%2002.mp3?raw=true",
+    "description": "Union Square-Chess Game:Union Square is a big chess hub and it’s located just outside the subway entrance near the northeast corner of 14th St. and Union Square West. Dozens of chess players sit on crates and use makeshift tables. They lure passersby with offers to try to beat them for $5 to $10 per game. Some players make a living from playing chess in this area. It is a way of relaxation in the New York City.",
+    "pictures": "https://raw.githubusercontent.com/ZhijunSong/MuseumofSound/main/assets/14th%20street/14thSt2.jpg"
+  },
+  {
+    "location": "Union Square",
+    "soundaddress": "https://github.com/ZhijunSong/MuseumofSound/blob/main/assets/14th%20street/14th%20St%2001.mp3?raw=true",
+    "description": "Union Square-Farmers Market: \n\nFarmer market is an essential elements for any city, a tradition as old as civilization itself. Union Square Greenmarket began with just a few farmers in 1976 and has grown exponentially. In peak season, 140 regional farmers, fishers, and bakers sell their products to a dedicated legion of city dwellers. It’s open Monday, Wednesday, Friday, and Saturday and you can find up to 60,000 shoppers wandering around the market. You can hear the sound of shoppers chatting with vendor in this clip.",
+    "pictures": "https://raw.githubusercontent.com/ZhijunSong/MuseumofSound/main/assets/14th%20street/14thSt1.jpg"
+  },
+  {
+    "location": "Union Square",
+    "soundaddress": "https://github.com/ZhijunSong/MuseumofSound/blob/main/assets/14th%20street/14th%20St.%2003mp3?raw=true",
+    "description": "The Metropolitan Transportation Authority (MTA) grants every musician the legal right to perform so you can find many of them inside subway stations.  In 2020, music in the subway disappeared because of the city lockdown. The MTA restarted its Music Under New York program, which organize some 350 performers at some of the most popular underground spots. It gives people the feeling that New York is returning to normal. Performance in the subway represents the street culture of New York City.",
+    "pictures": "https://raw.githubusercontent.com/ZhijunSong/MuseumofSound/main/assets/14th%20street/14thSt3.jpg"
+  },
+  {
+    "location": "34th Street : Greeley Square",
+    "soundaddress": "https://github.com/ZhijunSong/MuseumofSound/blob/main/assets/Ktown/34th%20st%2002.mp3?raw=true",
+    "description": "Greeley square park:\n\nGreeley square park is a triangular space that lies between West 32nd Street and West 33rd street in Midtown Manhattan. It serves as a rest area for the thousands of shoppers, a lunchroom for people who work in midtown, and a stage for musical performances. There are two tables for people to play ping pong. In this area, you can often hear fire truck and police car sirens.",
+    "pictures": "https://raw.githubusercontent.com/ZhijunSong/MuseumofSound/main/assets/Ktown/34thSt2.jpg"
+  },
+  {
+    "location": "Ktown Food Court",
+    "soundaddress": "https://github.com/ZhijunSong/MuseumofSound/blob/main/assets/Ktown/34th%20St.01.m4a?raw=true",
+    "description": "Korean town Food court:\n\nThere are high density of crowded restaurants, bars, and karaoke clubs in Koreatown. It is a major tourist attraction and the center of nightlife in Manhattan. This clip is from a food court in Koreatown and you can hear diners talking, Korean music playing",
+    "pictures": "https://raw.githubusercontent.com/ZhijunSong/MuseumofSound/main/assets/Ktown/34thSt1.jpg"
+  },
+  {
+    "location": "Time Square",
+    "soundaddress": "https://github.com/ZhijunSong/MuseumofSound/blob/main/assets/Time%20Square/42nd%20St.%2001mp3?raw=true",
+    "description": "Time Square:\n\nTimes Square is one of the world's most visited tourist attractions, drawing an estimated 50 million visitors annually. Approximately 330,000 people pass through Times Square daily and you can hear various languages being spoken.",
+    "pictures": "https://raw.githubusercontent.com/ZhijunSong/MuseumofSound/main/assets/Time%20Square/42ndSt1.jpg"
+  },
+  {
+    "location": "Time Square",
+    "soundaddress": "https://github.com/ZhijunSong/MuseumofSound/blob/main/assets/Time%20Square/42nd%20St.%2002.mp3?raw=true",
+    "description": "Street announcer in Time squareTime square is a famous tourist destination and has a lot of street announcers selling to tourists passing by. The sound comes from a street announcer selling discount tickets in Time Square.",
+    "pictures": "https://raw.githubusercontent.com/ZhijunSong/MuseumofSound/main/assets/Time%20Square/42ndSt2.jpg"
+  },
+  {
+    "location": "Time Square",
+    "soundaddress": "https://github.com/ZhijunSong/MuseumofSound/blob/main/assets/Time%20Square/42nd%20St%2003.m4a?raw=true",
+    "description": "Street performer in time square subway:\n\nTimes Square is a popular destination for busking due to the large flow of people passing through. The Metropolitan Transportation Authority (MTA)  grants every musician the legal right to perform. In 2020, music in the subway disappeared because of the city lockdown. MTA restarted its Music Under New York program, which organizes some 350 other performers at some of the most popular underground spots, to give people the feeling that New York is returning to normal. You can hear a slice of iconic New York street culture in this clip.",
+    "pictures": "https://raw.githubusercontent.com/ZhijunSong/MuseumofSound/main/assets/Time%20Square/42ndSt3.jpg"
+  }
+]
+
+
+let songs=[];
+let img=[];
 let nodeX;
-let clicked = false;
-let clickedItem = -1;
+let nodeY;
+let buttons=[];
 let cnv;
-
-
-
-
+let indexID =0;
 function preload(){
-    // fileName=[];
-    // for(let i =0;i<total-1;i++){
-    //   imports[i]=loadSound('assets/ChinatownErHu.m4a');
-    // backgroundImg[i]=loadImage('image/,,,,');
+  sound[0] =loadSound(mapdata.features[0].soundaddress,loaded);
 
-    // }
-    //canal st
-    imports[0] = loadSound('assets/Canal street/Canal St.01.m4a');
+  sound[0] =loadSound(mapdata.features[1].soundaddress,loaded);
+  sound[0] =loadSound(mapdata.features[2].soundaddress,loaded);
+  sound[0] =loadSound(mapdata.features[3].soundaddress,loaded);
+  sound[0] =loadSound(mapdata.features[4].soundaddress,loaded);
+  img.push(createImg(mapdata.features[0].pictures,'canalst'));
+  img.push(createImg(mapdata.features[1].pictures,'canalst'));
+  img.push(createImg(mapdata.features[2].pictures,'canalst'));
+  img.push(createImg(mapdata.features[3].pictures,'canalst'));
+  img.push(createImg(mapdata.features[4].pictures,'canalst'));
 
-    imports[1] = loadSound('assets/Canal street/Canal St.02.mp3');
-    imports[2] = loadSound('assets/Canal street/Canal St.03.m4a');
-    imports[3] = loadSound('assets/Canal street/Canal St.04.m4a');
-    imports[4] = loadSound('assets/Canal street/Canal St.05.m4a');
+  // songs[0] = loadSound("https://github.com/ZhijunSong/MuseumofSound/blob/main/assets/Canal%20street/Canal%20St.01m4a?raw=true",loaded);
+  // songs[1] = loadSound('https://github.com/ZhijunSong/MuseumofSound/blob/main/assets/Canal%20street/Canal%20St.01m4a?raw=true',loaded);
+  // songs[2] = loadSound('https://github.com/ZhijunSong/MuseumofSound/blob/main/assets/Canal%20street/Canal%20St.03m4a?raw=true',loaded);
+  // songs[3] = loadSound('https://github.com/ZhijunSong/MuseumofSound/blob/main/assets/Canal%20street/Canal%20St.04.m4a?raw=true',loaded);
+  // songs[4] = loadSound('assets/Canal street/CanalSt5.mp3',loaded);
+  // img.push(createImg('https://raw.githubusercontent.com/ZhijunSong/MuseumofSound/main/assets/Canal%20street/CanalSt2.jpg','canalst'));
+  // img.push(createImg('https://raw.githubusercontent.com/ZhijunSong/MuseumofSound/main/assets/Canal%20street/Canal%20St.%2001.png','canalst'));
+  // img.push(createImg('https://raw.githubusercontent.com/ZhijunSong/MuseumofSound/main/assets/Canal%20street/CanalSt3.jpg','canalst'));
+  // img.push(createImg('https://raw.githubusercontent.com/ZhijunSong/MuseumofSound/main/assets/Canal%20street/CanalSt4.jpg','canalst'));
+  // img.push(createImg('https://github.com/ZhijunSong/MuseumofSound/blob/main/assets/Canal%20street/Canal%20St.%2005.HEIC?raw=true','canalst'));
 
-    //14th
-    imports[5] = loadSound('assets/14th street/14th St 01.mp3');
-    imports[6] = loadSound('assets/14th street/14th St. 02.mp3');
-    imports[7] = loadSound('assets/14th street/14th St. 03.mp3');
+  // song[5] = loadSound('assets/14th street/14thSt1.mp3',loaded);
+  // song[6] = loadSound('assets/14th street/14thSt2.mp3',loaded);
+  // song[7] = loadSound('assets/14th street/14thSt3.mp3',loaded);
 
 
-    //34th
-  
-    imports[8] = loadSound('assets/Ktown/34th st 02.mp3');
-    imports[9] = loadSound('assets/Ktown/34th St.01.m4a');
-    imports[10] = loadSound('assets/14th street/14th St 01.mp3');
-    imports[11] = loadSound('assets/14th street/14th St 01.mp3');
+  // song[8] = loadSound('assets/Ktown/34thSt1.mp3',loaded);
+  // song[9] = loadSound('assets/Ktown/34thSt2.mp3',loaded);
+
+  // song[10] = loadSound('assets/Time Square/42ndSt1.mp3',loaded);
+  // song[11] = loadSound('assets/Time Square/42ndSt2.mp3',loaded);
+  // song[12] = loadSound('assets/Time Square/42ndSt3.mp3',loaded);
 
 }
+function setup() {
+  // cnv=createCanvas(0,0);
+  // cnv.parent('#canvas');
 
-function setup(){
-    cnv=createCanvas(windowWidth,windowHeight*2);
-    cnv.parent('canvas');
-    cnv.style("width:100%");
-    cnv.style("height:100%");
-    
-
-    for(let i = 0; i < 5; i++){
-      nodeY[i] = height/i
-    }
-      
-    nodeX = width/4;
-
-    //create sounds
-    
-    sounds[0] = new SoundNode(imports[0], 1, 'snvo', nodeX, nodeY[0]);
-    sounds[1] = new SoundNode(imports[1], 1, 'snvo', nodeX, nodeY[1]);
-    sounds[2] = new SoundNode(imports[2], 1, 'snvo', nodeX, nodeY[2]);
-    sounds[3] = new SoundNode(imports[3], 1, 'snvo', nodeX, nodeY[3]);
-    sounds[4] = new SoundNode(imports[4], 1, 'snvo', nodeX, nodeY[4]);
-  
-    sounds[0].active = true;
-    sounds[0].sound_id.play();
-}
-
-function draw(){
-  
-    background(0);
-    // background(50);
-    stroke(255, 208, 79);
-    strokeWeight(10);
-    line(nodeX, 0, nodeX, height);
+  for(let i=0;i<5;i++){
+    buttons[i] = createButton(' ');
+  }
+  // buttons.forEach(button => {
+  for(let i =0;i<buttons.length;i++){
+    let d = i;
+    buttons[i].mousePressed(function(){TogglePlay(i)});
+    buttons[i].parent('loc');
+    buttons[i].id('soundstop');
+  }
+  for(let i =0;i<img.length;i++){
+    img[i].parent('background');
+    img[i].id('backdrop')
+  }
    
-    sounds.forEach(sound => sound.display());
-    sounds.forEach(sound => sound.play());
-  
-    
 
+  background(51,0);
 
 
 }
 
-function windowResized() {
-  cnv=resizeCanvas(windowWidth, windowHeight);
+function loaded(){
+  console.log("loaded");
 }
 
-function mouseClicked(){
-  for(let i = 0; i < sounds.length; i++){
-    if(sounds[i].checkClick()){
-      clicked = true;
-      clickedItem = i;
+function TogglePlay(id){
+    if(!songs[id].isPlaying()){
+      songs[id].play(); 
+      songs[id].setVolume(0.5);
+      buttons[id].style('  transition: background-color 0.5s ease;');
     }else{
-      clicked =false;
+      songs[id].stop();
     }
-  }
-  if(clicked){
-  sounds.forEach(sound => sound.stop());
-  sounds[clickedItem].active = true;
-  sounds[clickedItem].sound_id.play();
-  clicked = false;
-  clickedItem = -1;
-
-  }
-  
-  
+    indexID= id;
+    console.log(indexID);
+ 
+    return indexID;
 }
-  
-class SoundNode{
-  constructor(sound_id, rel_pos, desc, x, y){
-      this.sound_id = sound_id;
-      this.rel_pos = rel_pos;
-      this.height = 50;
-      this.desc = desc;
-      this.active = false;
-      this.playing = false;
-      this.x = x;
-      this.y = y;
-      this.d = 50;
-      this.d2 = 100;
-      this.color = color(255, 214, 108)
+
+
+
+// console.log(location.description,stop);
+
+var app2=new Vue({
+  el:"#mapbox",
+  data:{  
+      counter:0,
+      location:`${mapdata[0].features[0].location}`,
+      description:`${mapdata[0].features[0].description}`,
+      stopName:`${mapdata[0].stop}`
+  },
+  methods:{
+    loadDatas(location){
+      console.log(this.locations);
+    },
+    increment(){
+      this.counter= indexID;
+      this.location=`${mapdata[0].features[this.counter].location}`;
+      this.description=`${mapdata[0].features[this.counter].description}`;
   }
-
-  display(){
-      if(!this.active){
-          stroke(this.color, 100);
-          strokeWeight(5);
-          fill(this.color, 100);
-          circle(this.x, this.y, this.d);
-          noFill();
-          circle(this.x, this.y, this.d2);
-      }else{
-          stroke(this.color);
-          strokeWeight(5);
-          fill(this.color);
-          circle(this.x, this.y, this.d2);
-      }
-  }
-
-  checkClick(){
-      let d = dist(this.x, this.y, mouseX, mouseY);
-          if(d <= this.d2){
-              return true;
-          }
-          else{
-            return false;
-          }
-      
-  }
-
-  play(){
-      if(this.active){
-          let m = map(mouseX, this.x, width, 0, this.sound_id.duration())
-          let pos = map(this.sound_id.currentTime(), 0, this.sound_id.duration(), this.x, width)
-          strokeWeight(5);
-          stroke(200);
-          line(this.x, this.y, width, this.y);
-          stroke(255);
-          line(this.x, this.y, pos, this.y);
-
-          if(mouseX < width && mouseX > this.x && mouseY < this.y + 20 && mouseY > this.y - 20){
-              circle(mouseX, this.y, 20);
-              if(mouseIsPressed){
-                  this.sound_id.jump(m, this.sound_id.duration)
-              }
-          }
-          
-      }
-  }
-
-  //for switch
-  stop(){
-    this.active = false
-    this.sound_id.stop();
-  }
-
-
-
-
 }
+
+})
+// var app3 = new Vue({
+
+// })
